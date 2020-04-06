e56a426a9b2baf13ca6205d411bab473e7901cf7
icygurkirat <gkiratbajwa@gmail.com>
2020-04-06 03:29:54 -0400

    Added parallel scan

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -14,10 +14,45 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
 }
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
-  // TODO: implement multi-threaded OpenMP scan
+  #pragma omp parallel
+  {
+    int tid = omp_get_thread_num(), p = omp_get_num_threads(), len = n/p;
+    
+    //indices for local section
+    int i = tid * len, j = (tid == p-1)?(n-1):(i+len-1);
+
+    //find local sum
+    long sum = (i==0?0:A[i-1]);
+    for(int ii = i+1; ii <= j; ii++)
+      sum += A[ii-1];
+    
+    //store this sum in global array
+    prefix_sum[i] = sum;
+
+    #pragma omp barrier
+
+    sum = 0;
+    for(int ii = 0; ii < tid; ii++)
+      sum += prefix_sum[ii*len];
+
+    #pragma omp barrier
+
+    //add this additional sum
+    prefix_sum[i] = sum + (i==0?0:A[i-1]);
+    for(int ii = i+1; ii <= j; ii++)
+      prefix_sum[ii] = prefix_sum[ii-1] + A[ii-1];
+
+  }
 }
 
-int main() {
+int main(int argc, char** argv) {
+  #ifdef _OPENMP
+    if(argc >= 2) {
+        int numThreads = atoi(argv[1]);
+        omp_set_num_threads(numThreads);
+    }
+	#endif
+
   long N = 100000000;
   long* A = (long*) malloc(N * sizeof(long));
   long* B0 = (long*) malloc(N * sizeof(long));
